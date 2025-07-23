d4893fdbd90e53df58391fdc301d8b9eb99440bc
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-23 17:35:50 +0200

    Improve stage9 and do not lower bitwidth of some unused data

--- velocity/utils/stages/compile_gpu_stage9.py
+++ velocity/utils/stages/compile_gpu_stage9.py
@@ -1,3 +1,4 @@
+import os
 import dace
 import argparse
 import utils.stages.common as common
@@ -6,7 +7,6 @@ from utils.input_to_gpu import input_to_gpu
 from utils.add_set_zero import add_set_zero
 from utils.make_flattened_data_to_input import make_flattened_data_to_non_transient_cpu_input, make_flattened_data_to_non_transient_gpu_input
 
-from utils.profiling_patches import remove_profiling_states, remove_sync_states, remove_sync_and_profiling_states, set_default_stream, insert_pre_reduction_sync, rm_reduntant_copies
 STAGE_ID = 9
 
 _allocation_names_to_comment_out = set()
@@ -20,6 +20,7 @@ def optimization_action(sdfg):
     shallow_copy_used_structs = ["p_prog", "p_int", "p_metrics", "p_patch", "p_diag"]
     deflatten_used_structs = ["p_diag"]
     _allocation_names_to_comment_out =  change_flatten_lib_to_shallow_copy(sdfg, shallow_copy_used_structs, deflatten_used_structs)
+
     sdfg.validate()
     #add_set_zero(sdfg, "gpu_maxvcfl_arr")
     sdfg.validate()
@@ -34,10 +35,6 @@ def optimization_action(sdfg):
         make_flattened_data_to_non_transient_gpu_input(sdfg)
         sdfg.validate()
 
-    set_default_stream(sdfg)
-    remove_sync_and_profiling_states(sdfg)
-    insert_pre_reduction_sync(sdfg)
-    rm_reduntant_copies(sdfg)
     return sdfg
 
 def main():
@@ -70,8 +67,14 @@ def main():
     if args.compile:
         # Read back the written files as we prepare for compilation.
         sdfgs = {name: dace.SDFG.from_file(common.stage_output(name, STAGE_ID)) for name in names}
-        assert _allocation_names_to_comment_out is not None and _allocation_names_to_comment_out != set()
-        common.compile_action(STAGE_ID, sdfgs, True, _allocation_names_to_comment_out, True)
+        #assert _allocation_names_to_comment_out is not None and _allocation_names_to_comment_out != set()
+        common.compile_action(
+            stage=STAGE_ID,
+            sdfgs=sdfgs,
+            lib=True,
+            allocation_names_to_comment_out=_allocation_names_to_comment_out,
+            use_openacc_stream=False
+        )
 
 if __name__ == "__main__":
     main()
